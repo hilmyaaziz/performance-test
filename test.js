450938b3e00b601f989cd47cdce2ed3d90b1e2c5
hilmyaziz <hilmy.aziz@fitaja.id>
2023-04-17 22:06:10 +0700

    add workflows

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export let options = {
     stages: [
-      { duration: '5m', target: 100 },
-      { duration: '8m', target: 100 },
-      { duration: '5m', target: 0 },
+      { duration: '1m', target: 100 },
+      { duration: '30s', target: 100 },
+      { duration: '45s', target: 50 },
     ],
   };
 
